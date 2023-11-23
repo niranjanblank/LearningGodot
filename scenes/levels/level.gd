990bf464737d961eb3c9800d1b51fd0d418911d6
Niranjan Shah <niranjanshah474@gmail.com>
2023-11-23 21:28:38 +1100

    direction added to laser and grenade

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -19,15 +19,18 @@ func _on_gate_player_exited_gate():
 	print("player exited")
 
 
-func _on_player_laser(pos):
+func _on_player_laser(pos, direction):
 	var laser = laser_scene.instantiate()
 	# update laser position
 	laser.position = pos
+	laser.rotation_degrees = rad_to_deg	(direction.angle()) + 90
+	laser.direction = direction
 	$Projectiles.add_child(laser)
 	
 
-func _on_player_grenade(pos):
-	var grenade = grenade_scene.instantiate()
+func _on_player_grenade(pos, direction):
+	var grenade = grenade_scene.instantiate() as RigidBody2D
 	# update laser position
 	grenade.position = pos
+	grenade.linear_velocity = direction * grenade.speed
 	$Grenades.add_child(grenade)
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,8 +3,8 @@ extends CharacterBody2D
 var can_laser: bool = true
 var can_grenade: bool = true
 
-signal laser(pos)
-signal grenade
+signal laser(pos, direction)
+signal grenade(pos, direction)
 
 func _process(_delta):
 	
@@ -13,22 +13,28 @@ func _process(_delta):
 	velocity = direction * 300
 	move_and_slide()
 	
+	# rotate
+	look_at(get_global_mouse_position())
+	
 	# shooting laser
 	if Input.is_action_just_pressed('primary action') and can_laser:
+		can_laser = false
 		# randomly select marker
 		var laser_markers = $LaserStartPositions.get_children()
 		var selected_laser = laser_markers[randi() % laser_markers.size()]
-		can_laser = false
+		var player_direction = (get_global_mouse_position() - position).normalized()
 		$Timer.start()
 		# emit the position we selected 
-		laser.emit(selected_laser.global_position)
+		laser.emit(selected_laser.global_position, player_direction)
 		
 	if Input.is_action_just_pressed('secondary action') and can_grenade:
+		can_grenade = false
 		var grenade_markers = $GrenadeStartPosition.get_children()
 		var selected_grenade = grenade_markers[0]
-		can_grenade = false
+		var pos = selected_grenade.global_position
+		var player_direction = (get_global_mouse_position() - position).normalized()
 		$GrenadeReloadTimer.start()
-		grenade.emit(selected_grenade.global_position)
+		grenade.emit(pos,player_direction)
 
 
 func _on_timer_timeout():
