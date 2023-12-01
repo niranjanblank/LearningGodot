24f4a93afebe8e90e975f53fd6a1a4d6db641d14
Niranjan Shah <niranjanshah474@gmail.com>
2023-12-01 18:03:38 +1100

    transition between levels added

--- scenes/levels/inside.gd
+++ scenes/levels/inside.gd
@@ -1,7 +1,9 @@
 extends LevelParent
 
-
+var outside_level_scene: PackedScene = preload('res://scenes/levels/outside.tscn')
 
 func _on_exit_gate_area_body_entered(_body):
 	var tween = create_tween()
 	tween.tween_property($Player,"speed", 0 , 0.5)
+	# get_tree().change_scene_to_file('res://scenes/levels/outside.tscn')
+	get_tree().change_scene_to_packed(outside_level_scene)
